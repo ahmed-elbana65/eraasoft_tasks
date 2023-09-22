9bb2d641e58280c2268a3e4fb635e3dc042fe840
ahmed-elbana65 <65ahmedelbana@gmail.com>
2023-09-22 09:35:29 +0300

    task 9 java

---
+++ task 9/index.js
@@ -0,0 +1,229 @@
+// 1
+
+// function printArray(number) {
+//     var Array = [];
+  
+//     for(var i = 1; i <= number;i++) {
+//         console.log(i);
+     
+//     }
+  
+//     return Array;
+//   }
+
+//   console.log(printArray(5));
+
+
+
+// 2
+
+
+
+
+
+
+// function swap(a , b) {
+
+//     return[b,a]
+// }
+
+// console.log(swap(100,200));
+
+
+
+// 3
+
+// function animals(chickens, cows, pigs) {
+// 	 sum = (chickens * 2) + (cows * 4) + (pigs * 4);
+// 	return sum;
+// }
+
+// console.log(animals(2,3,5));
+
+
+
+
+// 4
+
+//المفروض لييها حل تانى بس انا مش فاهمه 
+
+// function check(a, b) {
+// 	if (a==false)
+// {
+    // return false
+// }
+// 	else if (b==false)
+// {
+    // return false
+// }
+// 	else if (a==true && b==true)
+// {
+    // return true
+// }
+// }
+
+// console.log(check(true,false));
+
+
+
+// 5
+
+
+// function check(a,b) {
+//     if (a===b) {
+//         return true
+        
+//     }
+//     else {
+//         return false
+//     }
+    
+// }
+// console.log(check(4,5));
+
+
+// 6
+
+
+// function foot(wins, drawes, loses) {
+// 	 sum = (wins * 3) + (drawes) + (loses * 0);
+// 	return sum;
+// }
+
+// console.log(foot(3,4,2));
+
+
+
+// 7
+
+
+// function convert(hours, minutes) {
+// 	 sum = (hours * 60*60) + (minutes * 60);
+// 	return sum;
+// }
+
+// console.log(convert(1,3));
+
+
+
+// 8
+
+
+// function isseven(num) {
+//     if (num==7) {
+//         return true
+        
+//     }
+//     else{
+//         return false
+//     }
+    
+// }
+// console.log(isseven(7));
+
+
+
+// 9
+
+// function check(a,b) {
+//     if (a===b) {
+//         return true
+        
+//     }
+//     else {
+//         return false
+//     }
+    
+// }
+// console.log(check(1,true));
+
+
+
+// 10
+
+// function profitableGamble(prob,prize,pay) {
+
+//     if (prob*prize > pay) {
+//         return true
+        
+//     }
+//     else{
+//         return false
+//     }
+    
+// }
+// console.log(profitableGamble(0.2, 50, 9));
+
+
+
+// 11
+
+
+// function boolToString(a) {
+//     if (a==true) {
+//         return "true"
+        
+//     }
+//     else{
+//         return "false"
+//     }
+    
+// }
+
+// console.log(boolToString(false));
+
+
+// 12
+
+
+
+//  arrowFunc=(a)=> {
+//     return a
+// }
+
+// console.log(arrowFunc('ahmed'));
+
+
+
+
+// 13
+
+
+// function frames(a,b) {
+
+//     return a*b*60
+    
+// }
+// console.log(frames(10,25));
+
+
+
+
+// 14
+
+// function convert(a) {
+//     return eval(a)
+    
+// }
+// console.log(convert("15-5"));
+
+
+
+
+// 1
+
+
+
+// 15
+
+// function greeting(name){
+//     if(name == "Mubashir") {
+//       return "Hello, my Love!";
+//     }
+//     else{
+
+//         return "Hello, " + name + "!";
+//     }
+    
+//   }
+//   console.log(greeting("gfdhs"));
